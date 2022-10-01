d1107063e7eebfcda34ff8bb0cee9094c6fade7a
Victor Hurtado <victomhl@gmail.com>
2022-10-01 10:29:08 -0400

    Add Bootstrap, and make structure changes to the project folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react"
 import Confetti from "react-confetti"
 
-import Die from "../components/Die"
-import useTenzies from "../components/useTenzies"
+import Die from "./components/Die"
+import useTenzies from "./hooks/useTenzies"
 
 function App() {
 
